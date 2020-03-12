efbc76d3ce4223efd52ddfab5d9a2c3d2b1f4e6c
blandineboudet <47861016+blandineboudet@users.noreply.github.com>
2020-03-12 16:49:03 +0100

    Add files via upload

---
+++ Blockchain/Client App/Back/FirstNetwork Fabric Blockchain/network.js
@@ -0,0 +1,205 @@
+'use strict';
+
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+
+
+// capture network variables from config.json
+const configPath = path.join(process.cwd(), '/config.json');
+const configJSON = fs.readFileSync(configPath, 'utf8');
+const config = JSON.parse(configJSON);
+let connection_file = config.connection_file;
+let userName = config.userName;
+let gatewayDiscovery = config.gatewayDiscovery;
+
+// connect to the connection file
+const ccpPath = path.join(process.cwd(), connection_file);
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+
+// create deviceidentity transaction
+exports.createDeviceIdentity = async function(key, pubinfo, hashid, gatewayid, state) {
+    let response = {};
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists(userName);
+        if (!userExists) {
+            console.log('An identity for the user ' + userName + ' does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first';
+            return response;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: userName, discovery: gatewayDiscovery });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabdeviceidentity');
+
+        // Submit the specified transaction.
+        // createDevice transaction - requires 5 argument, ex: ('createDeviceIdentity', '1', '123456abcd', '654321dcba', 'gatewaytest', 'newidentity')
+        await contract.submitTransaction('createDeviceIdentity', key, pubinfo, hashid, gatewayid, state);
+        console.log('Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+        response.msg = 'createDeviceIdentity Transaction has been submitted';
+        return response;
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        response.error = error.message;
+        return response;
+    }
+};
+
+// change deviceidentity state transaction
+exports.changeDeviceIdentityState = async function(key, newState) {
+    let response = {};
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists(userName);
+        if (!userExists) {
+            console.log('An identity for the user ' + userName + ' does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first';
+            return response;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: userName, discovery: gatewayDiscovery });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabdeviceidentity');
+
+        // Submit the specified transaction.
+        // changeDeviceIdentityState transaction - requires 2 args , ex: ('changeDeviceIdentityState', '1', 'used')
+        await contract.submitTransaction('changeDeviceIdentityState', key, newState);
+        console.log('Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+        response.msg = 'changeDeviceIdentityState Transaction has been submitted';
+        return response;
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        response.error = error.message;
+        return response;
+    }
+};
+
+// query all devicesidentity transaction
+exports.queryAllDevicesIdentity = async function() {
+
+    let response = {};
+    try {
+        console.log('queryAllDevicesIdentity');
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists(userName);
+        if (!userExists) {
+            console.log('An identity for the user ' + userName + ' does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first';
+            return response;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: userName, discovery: gatewayDiscovery });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabdeviceidentity');
+
+        // Evaluate the specified transaction.
+        // queryAllDevicesIdentity transaction - requires no arguments, ex: ('queryAllDevicesIdentity')
+        const result = await contract.evaluateTransaction('queryAllDevicesIdentity');
+        //console.log('check6');
+        //console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+
+        return result;
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        response.error = error.message;
+        return response;
+    }
+};
+
+// query the deviceidentity identified by key
+exports.querySingleDeviceIdentity = async function(key) {
+
+    let response = {};
+    try {
+        console.log('querySingleDeviceIdentity');
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists(userName);
+        if (!userExists) {
+            console.log('An identity for the user ' + userName + ' does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first';
+            return response;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: userName, discovery: gatewayDiscovery });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabcardevicesidentity');
+
+        // Evaluate the specified transaction.
+        // querySingleDeviceIdentity transaction - requires 1 argument, ex: 'querySingleDeviceIdentity('1')'
+        console.log(key);
+        const result = await contract.evaluateTransaction('querySingleDeviceIdentity', key);
+        //console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+
+        return result;
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        response.error = error.message;
+        return response;
+    }
+};
