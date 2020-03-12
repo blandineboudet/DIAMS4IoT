aea5f948d1fc1c2d3640f6f8ec7e07043912f7e2
blandineboudet <47861016+blandineboudet@users.noreply.github.com>
2020-03-12 16:57:11 +0100

    Add files via upload

---
+++ Blockchain/Client App/Back/FirstNetwork Fabric Blockchain/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const FabDeviceIdentity = require('./lib/fabdeviceidentity');
+
+module.exports.FabDeviceIdentity = FabDeviceIdentity;
+module.exports.contracts = [ FabDeviceIdentity ];
